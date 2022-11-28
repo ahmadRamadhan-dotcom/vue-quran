89e472877964b5d467ea2e07ca867067c494d1a0
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-28 15:32:25 +0700

    chore: responsive surah card

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <p class="text-red-500">test</p>
-    <HelloWorld />
-  </div>
+  <main
+    class="mx-auto w-11/12 sm:w-[96%] md:w-10/12 lg:w-[80%] xl:w-[1100px] 2xl:w-[1300px] mt-10 grid place-content-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
+  >
+    <SurahCard />
+    <SurahCard />
+    <SurahCard />
+    <SurahCard />
+  </main>
 </template>
 
 <script setup>
-import HelloWorld from "~/components/HelloWorld.vue";
+import SurahCard from "~/components/home/SurahCard.vue";
 </script>
---
+++ src/components/home/SurahCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <article
+    class="border border-[#464b50] hover:border-[#2ca4ab] group flex justify-between items-center transition duration-200 rounded-[.25rem] h-[4.4em] p-5"
+  >
+    <div class="flex gap-5" id="surah_preview_left">
+      <div
+        class="bg-[#343a40] group-hover:bg-[#2ca4ab] rotate-45 group-hover:text-black rounded-sm w-10 h-10 grid place-content-center place-items-center"
+      >
+        <p class="rotate-[-45deg] font-bold">1</p>
+      </div>
+      <div>
+        <p id="surah_name" class="text-base font-bold">Al-Fatihah</p>
+        <p
+          id="meaning"
+          class="text-[#777] font-bold text-[12px] group-hover:text-[#2ca4ab]"
+        >
+          Pembukaan
+        </p>
+      </div>
+    </div>
+    <div id="surah_preview_right">
+      <p class="text-[13px]">الفاتحة</p>
+      <p class="text-[#777] font-bold text-[12px] group-hover:text-[#2ca4ab]">
+        7 Ayat
+      </p>
+    </div>
+  </article>
+</template>
