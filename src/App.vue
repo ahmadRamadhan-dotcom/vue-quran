32e5c32f21c7433a28cde81f67920f367d244d99
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-28 13:52:03 +0700

    chore: absolute path for src directory

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
     <p class="text-red-500">test</p>
+    <HelloWorld />
   </div>
 </template>
+
+<script setup>
+import HelloWorld from "~/components/HelloWorld.vue";
+</script>
