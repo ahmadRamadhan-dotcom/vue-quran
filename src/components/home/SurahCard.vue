136fd5e9c657c2cced6275e634ae543b5079a36f
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-28 16:00:08 +0700

    chore: fetch data from api

--- src/components/home/SurahCard.vue
+++ src/components/home/SurahCard.vue
@@ -1,28 +1,36 @@
 <template>
   <article
+    v-for="(data, index) in surahData"
+    :key="index"
     class="border border-[#464b50] hover:border-[#2ca4ab] group flex justify-between items-center transition duration-200 rounded-[.25rem] h-[4.4em] p-5"
   >
     <div class="flex gap-5" id="surah_preview_left">
       <div
         class="bg-[#343a40] group-hover:bg-[#2ca4ab] rotate-45 group-hover:text-black rounded-sm w-10 h-10 grid place-content-center place-items-center"
       >
-        <p class="rotate-[-45deg] font-bold">1</p>
+        <p class="rotate-[-45deg] font-bold">{{ data.nomor }}</p>
       </div>
       <div>
-        <p id="surah_name" class="text-base font-bold">Al-Fatihah</p>
+        <p id="surah_name" class="text-base font-bold">{{ data.nama_latin }}</p>
         <p
           id="meaning"
           class="text-[#777] font-bold text-[12px] group-hover:text-[#2ca4ab]"
         >
-          Pembukaan
+          {{ data.arti }}
         </p>
       </div>
     </div>
     <div id="surah_preview_right">
-      <p class="text-[13px]">الفاتحة</p>
+      <p class="text-[13px]">{{ data.nama }}</p>
       <p class="text-[#777] font-bold text-[12px] group-hover:text-[#2ca4ab]">
-        7 Ayat
+        {{ data.jumlah_ayat }} Ayat
       </p>
     </div>
   </article>
 </template>
+
+<script setup>
+const props = defineProps({
+  surahData: Array,
+});
+</script>
